46186017676f737f8accdb5a079842d6845afacd
vcoppe <vianney.coppe@gmail.com>
2026-03-22 12:51:42 +0100

    try running server with graph in RAM

--- server.sh
+++ server.sh
@@ -4,8 +4,6 @@ mkdir -p logs
 
 cp config.yml /data/config.yml
 
-sed -i 's/RAM_STORE/MMAP/g' /data/config.yml
-
 docker run \
     -v /data:/data:ro \
     -v ./graph-cache:/graphhopper/graph-cache \
@@ -13,4 +11,4 @@ docker run \
     -p 8989:8989 \
     -p 8990:8990 \
     graphhopper \
-    -c "java -Xmx90g -XX:+UseZGC -jar *.jar server /data/config.yml"
+    -c "java -Xmx120g -XX:+UseZGC -jar *.jar server /data/config.yml"
